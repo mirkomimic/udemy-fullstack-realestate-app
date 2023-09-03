8b7c2b8008c3e1808f1088150a4a95ae4b2d635f
mirkoMac <mirkomimic88@gmail.com>
2023-09-03 07:49:46 +0200

    add listing, flash msg

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $fillable = ['beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'];
 }
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,15 +1,33 @@
 <template>
   <Link href="/">Main Page</Link>&nbsp;
   <Link href="/hello">Show Page</Link>
-  <div>The page with time {{ timer }}</div>
+  <!-- <div>The page with time {{ timer }}</div> -->
 
-  <slot></slot>
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+
+  <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
-import { ref } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+// import { ref } from "vue";
 
-const timer = ref(0);
-setInterval(() => timer.value++, 1000)
+// const timer = ref(0);
+// setInterval(() => timer.value++, 1000)
+
+const page = usePage();
+// for complex logic that includes reactive data, it is recommended to use a computed property
+const flashSuccess = computed(() => {
+  return page.props.flash.success;
+})
 </script>
+
+<style scoped>
+.success {
+  background-color: green;
+  color: white;
+}
+</style>
